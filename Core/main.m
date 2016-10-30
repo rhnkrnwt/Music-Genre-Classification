1d92dba36154916110ce60c956499fda0d5f5337
hensden <karnawat.rohan18@gmail.com>
2016-10-30 14:10:06 +0530

    Feature extraction refined

--- Core/main.m
+++ Core/main.m
@@ -6,7 +6,7 @@
 nRow = 1;
 nCol = 135;
 
-dataset = zeros(nRow*400,nCol); % Fiternal dataset
+dataset = zeros(nRow*500,nCol); % Fiternal dataset
 
 % easily add genres, increment number of rows by 15 * 100
 genre = dir('pop/*.au');
@@ -16,15 +16,23 @@ end
 
 genre = dir('jazz/*.au');
 for iter = 1:length(genre)
-    dataset(101 + iter,:) = mfcc('jazz',genre(iter).name);
+    dataset(100 + iter,:) = mfcc('jazz',genre(iter).name);
 end
 
 genre = dir('metal/*.au');
 for iter = 1:length(genre)
-    dataset(201 + iter,:) = mfcc('metal',genre(iter).name);
+    dataset(200 + iter,:) = mfcc('metal',genre(iter).name);
 end
 
 genre = dir('classical/*.au');
 for iter = 1:length(genre)
-    dataset(301 + iter,:) = mfcc('classical', genre(iter).name);
+    dataset(300 + iter,:) = mfcc('classical', genre(iter).name);
 end
+
+genre = dir('hiphop/*.au');
+for iter = 1:length(genre)
+    dataset(400 + iter,:) = mfcc('hiphop', genre(iter).name);
+end
+
+
+principal_data = red_dim(dataset);
